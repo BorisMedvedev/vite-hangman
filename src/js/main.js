b62f17f0e3fc72ee0ad4227b522824b3b66a968a
John Doe <johndoe@example.com>
2024-01-20 17:42:48 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
 import '../css/style.css';
 
 const toggleDarkMode = document.getElementById('toggleDarkMode');
+const htmlElement = document.documentElement;
+
+if (localStorage.getItem('mode') === 'dark') {
+  htmlElement.classList.add('dark');
+  toggleDarkMode.checked = true;
+}
 
 toggleDarkMode.addEventListener('input', () => {
-  const htmlEltmtnt = document.documentElement;
-  htmlEltmtnt.classList.toggle('dark');
+  htmlElement.classList.toggle('dark');
+
+  if (htmlElement.classList.contains('dark')) {
+    localStorage.setItem('mode', 'dark');
+  } else {
+    localStorage.setItem('mode', 'light');
+  }
 });
+
+
+const generateWords = () => {
+  const words = ['природа', 'автомобиль', 'солнце',
+    'дом', 'кошка', 'книга', 'яблоко', 'стул', 'компьютер', 'музыка'];
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  }
+};
+
