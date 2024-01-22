dc779623504bd2eeb9f99801a06566c350136a66
John Doe <johndoe@example.com>
2024-01-22 09:31:28 +0300

    fix

--- src/js/modules/createImg.js
+++ src/js/modules/createImg.js
@@ -1,7 +1,7 @@
 export const createImg = () => {
   const image = document.createElement('img');
   image.classList.add('hagman-image');
-  image.src = '/images/hg-0.png';
+  image.src = 'images/hg-0.png';
   image.alt = 'hagman image';
   image.id = 'hagmanImg';
 
