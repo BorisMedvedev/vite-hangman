fd21ec5fb8d148e52d6eedd8b7ad0facaa44fb8c
John Doe <johndoe@example.com>
2024-01-22 08:59:23 +0300

    fix

--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -11,13 +11,13 @@ export const startGame = () => {
   const logo = document.querySelector('.logo');
   const image = createImg();
   const keyboardWrapper = createKeyboard(ALPHABET);
+  generateWords(WORLD);
   const pseudoLetters = createPseudoLetters(sessionStorage.getItem('world'));
   const cancelBtn = document.createElement('button');
   logo.classList.add('logo-min');
   cancelBtn.classList.add('cancel-btn');
   cancelBtn.textContent = 'Выйти';
   game.innerHTML = '';
-  generateWords(WORLD);
   game.prepend(image);
   game.append(pseudoLetters.wrapper,
       pseudoLetters.lives,
